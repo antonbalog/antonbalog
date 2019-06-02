5bfe66bdb3bc004ac38260e654d10267d0423119
Anton Balog <anton.balog@o2.sk>
2019-06-02 19:44:31 +0200

    Just small corrections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }  from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
+// import Layout from './components/Layout';
+import Header from './components/Header'
+import SideDrawer from './components/SideDrawer';
+import Footer from './components/Footer'; 
+
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
+
+import './App.scss';
+
+const App = () => {
+  const [active, activate] = useState(false);
+
+  const activation = () => {
+    return activate(!active);
+  };
+
+  const [hover, hovered] = useState(':)');
+
+  const hovering = (hover) => {
+    return hovered(hover);
+  };
+
+  let label;
+
+  if (active) {
+    label = 'CLOSE';
+  } else {
+    label = 'MENU';
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header 
+          active={active}
+          activationHandler={activation}
+          labelHandler={label}
+          hoverHandler={hovering}
+        />
+        <Route path='/' component={Home} exact />
+        <Route path='/blog' component={Blog} />
+        <Route path='/work' component={Work} />
+        <Route path='/contact' component={Contact} />
+        <SideDrawer active={active} hover={hover}>{hover}</SideDrawer>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
